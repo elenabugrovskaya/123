85cd8ffae5a050e33c8367df876be39da23cf2bf
Helen <helen@Mac.local>
2022-10-15 23:53:05 +0700

    Pogram

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string useername = Console.ReadLine();
+if (useername == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(useername);
+}
