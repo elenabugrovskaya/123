f4bb31d608f4c70037aace6283dab43a0d7ea0e8
Helen <helen@Mac.local>
2022-10-15 23:18:02 +0700

    Program

--- Example003_sum/Program.cs
+++ Example003_sum/Program.cs
@@ -1,5 +1,6 @@
 ﻿int numberA = new Random().Next(1, 10); // 1 2 3 ... 9
-Console.Write(numberA);
+Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
