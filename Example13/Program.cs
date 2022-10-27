b840f253729a75aee312da9789361809fcc0ea2c
Helen <helen@Mac.local>
2022-10-27 23:04:09 +0700

    Example13

--- Example13/Program.cs
+++ Example13/Program.cs
@@ -1,9 +1,13 @@
 ﻿// Факториал
 
-int Factorial (int n)
+double Factorial (int n)
 {
     // 1! = 1, 0! = 1
     if (n == 1) return 1;
     else return n * Factorial(n-1);
 }
-Console.WriteLine(Factorial(3));
+//Console.WriteLine(Factorial(3));
+for (int i=1; i<40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
